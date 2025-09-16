5c02b9d3156fe4f4f655fd8f47aebd64f2597ee0
unknownemperork <unknownemperork@gmail.com>
2025-09-16 08:05:09 +0000

    Updated Designs

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,8 +1,8 @@
 const setupBanners = {
-    helpBanner : 'https://ibb.co/4ZmyRZRv',
-    verificationBanner : 'https://ibb.co/FkRtdkKw',
+    helpBanner : 'https://i.ibb.co/fY1RdYd6/Emberlyn.png',
+    verificationBanner : 'https://i.ibb.co/cK4dsK1F/Verfication.png',
     ticketBanner : 'https://i.ibb.co/DfL11Rdf/Tickets.png',
-    autovcBanner : 'https://ibb.co/ZksdJyS',
+    autovcBanner : 'https://i.ibb.co/VPyVNXx/AutoVC.png',
 };
 
 module.exports = setupBanners;
